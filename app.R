755a7937921974a81cfc49918dc23b5549760e59
hannah-merseal <hmm5708@psu.edu>
2020-04-23 17:08:51 -0400

    testing

--- app.R
+++ app.R
@@ -2,6 +2,12 @@ library(psychTestR)
 library(shiny)
 
 options <- test_options("Jazz Demo", admin_password = "rutabaga", researcher_email = "hmerseal@psu.edu")
+
+save_time_started <- code_block(fun = function(state, ...) {
+  set_global(key = "time.started",
+             value = Sys.time(),
+             state = state)
+})
   
 demographic <- join(
   one_button_page("Thanks for participating! First, you'll be entering some information about your musical background."),
@@ -12,8 +18,20 @@ demographic <- join(
   text_input_page("improv.train", "How many years of formal improvisation training have you had?", one_line = TRUE, save_answer = TRUE),
   text_input_page("theory.train", "How many years of formal music theory training have you had?", one_line = TRUE, save_answer = TRUE),
   slider_page("music.listen", "How many hours a week do you spend listening to music?", 0, 100, 0, save_answer = TRUE), 
-  final_page("Thank you for participating!")
+  elt_save_results_to_disk(complete = TRUE)
   )
 
-jazz.choice <- make_test(elts = demographic, opt = options, custom_admin_panel = NULL)
+compute_time_taken <- code_block(function(state, ...) {
+  time_taken <- Sys.time() - get_global("time_started", state)
+  msg <- paste0("Time taken: ", format(time_taken, digits = 3))
+  elt_save_results_to_disk(complete = TRUE)
+})
+
+jazz.choice <- make_test(join(
+  save_time_started,
+  demographic,
+  compute_time_taken,
+  final_page("Thank you for participating!")),
+  opt = options, custom_admin_panel = NULL)
+
 shiny::runApp(jazz.choice)
